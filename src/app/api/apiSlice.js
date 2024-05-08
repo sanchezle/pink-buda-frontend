8ad1ee733c039533d3423970d8107a2c22fbfb7a
sanchezle <sanchezle21@gmail.com>
2024-05-08 14:52:14 +0200

    https to baseUrl of aws bs

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://barcelove-env.eba-vkqq7dpr.eu-west-3.elasticbeanstalk.com/',
+    baseUrl: 'https://barcelove-env.eba-vkqq7dpr.eu-west-3.elasticbeanstalk.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
