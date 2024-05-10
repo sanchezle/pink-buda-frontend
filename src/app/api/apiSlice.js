e289ce0a08498fc62cb418f5f82025f2ba55be46
sanchezle <sanchezle21@gmail.com>
2024-05-10 18:56:57 +0200

    new backend url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://barcelove2-env.eba-fvp7hrw3.eu-west-3.elasticbeanstalk.com/',
+    baseUrl: 'https://barcelove.azurewebsites.net/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
