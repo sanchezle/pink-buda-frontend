925f4852b6ba8521f673172fb5e52686bb9333e0
sanchezle <sanchezle21@gmail.com>
2024-05-26 17:48:03 +0200

    navbar hidding and about formated

--- src/components/public_components/AboutPage.js
+++ src/components/public_components/AboutPage.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import './AboutPage.css'; // You can create this CSS file to style the component
+import './AboutPage.css'; // Import the CSS file to style the component
 
 const AboutPage = () => {
   return (
     <div className="about-page">
-      <h1 className="title">Acerca de Nosotros</h1>
-      <p className="subtitle">Seriedad en el Sabor</p>
-      <p className="text">
-        En Pink Buda Burgers, no tomamos las hamburguesas a la ligera. Desde nuestro humilde comienzo en 2023 en el corazón de Barcelona, nos hemos dedicado a crear las hamburguesas más deliciosas y satisfactorias que puedas encontrar. Nuestro eslogan lo dice todo: <strong>"We Are Serious About Smash"</strong>.
-      </p>
-      {/* Add more content here */}
+      <div className="content">
+        <h1 className="title">Acerca de Nosotros</h1>
+        <p className="subtitle">Seriedad en el Sabor</p>
+        <p className="text">
+          En Pink Buda Burgers, no tomamos las hamburguesas a la ligera. Desde nuestro humilde comienzo en 2023 en el corazón de Barcelona, nos hemos dedicado a crear las hamburguesas más deliciosas y satisfactorias que puedas encontrar. Nuestro eslogan lo dice todo: <strong>"We Are Serious About Smash"</strong>.
+        </p>
+        <p className="text">
+          <strong>Nuestra Historia</strong><br />
+          Fundada por un grupo de amigos apasionados por la comida y la buena compañía, Pink Buda Burgers nació con la visión de ofrecer algo más que una simple hamburguesa. Queríamos crear una experiencia culinaria que dejara una impresión duradera en nuestros clientes. Así que nos pusimos manos a la obra, experimentando con ingredientes frescos y sabores audaces.
+        </p>
+        <p className="text">
+          <strong>Nuestro Compromiso</strong><br />
+          <em>Calidad Superior:</em> Utilizamos carne de res de primera calidad, sazonada y cocida a la perfección. Cada hamburguesa es una obra maestra de sabor y textura.<br />
+          <em>Variedad Creativa:</em> Nuestro menú ofrece una amplia variedad de hamburguesas, desde las clásicas hasta las más atrevidas. ¿Quieres una “Smash Burger” con queso fundido y cebolla caramelizada? ¡Lo tenemos!<br />
+          <em>Ambiente Acogedor:</em> En Pink Buda Burgers, no solo servimos comida, sino también sonrisas. Nuestro personal amable y el ambiente relajado te harán sentir como en casa.
+        </p>
+      </div>
     </div>
   );
 };
 
 export default AboutPage;
+
--- src/components/public_components/Navbar.js
+++ src/components/public_components/Navbar.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from './images/logonav2.png';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+
   const handleClick = () => setClick(!click);
   const closeMenu = () => setClick(false);
 
+  useEffect(() => {
+    let lastScrollTop = 0;
+
+    const handleScroll = () => {
+      let scrollTop = window.scrollY;
+      if (scrollTop > lastScrollTop) {
+        // downscroll code
+        setIsHidden(true);
+      } else {
+        // upscroll code
+        setIsHidden(false);
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='header'>
+    <div className={`header ${isHidden ? 'hidden' : ''}`}>
       <nav className='navbar'>
         <a href='/' className='logo'>
           <img src={logo} alt='logo' />
@@ -25,7 +48,6 @@ const Navbar = () => {
           )}
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
-
           <li className='nav-item'>
             <Link to='/login' onClick={closeMenu}>Login</Link>
           </li>
