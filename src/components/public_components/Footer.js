ff4856498570cefed6757e19175b47070d90459a
sanchezle <sanchezle21@gmail.com>
2024-05-26 19:31:26 +0200

    acento en menú, footer 2024,  facebok icon2

--- src/components/public_components/Footer.js
+++ src/components/public_components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 const phoneIconStyle = {
     fontSize: '50px',
